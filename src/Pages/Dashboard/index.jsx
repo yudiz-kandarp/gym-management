4b1759ac97f3995b14cb59bcf94857ae4340f51a
yudiz-dhruv <dhruv.parmar@yudiz.com>
2023-06-16 17:00:33 +0530

    test

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -1,27 +1,27 @@
 import PageTitle from 'Components/Page-Title'
 import Wrapper from 'Components/wrapper'
 import { getDashboardStatistics } from 'Query/Dashboard/dashboard.query'
-import { parseParams } from 'helpers'
+// import { parseParams } from 'helpers'
 import React, { useState } from 'react'
 import { useQuery } from 'react-query'
 
 export default function Dashboard() {
-  const parsedData = parseParams(location.search)
+  // const parsedData = parseParams(location.search)
   const [data, setData] = useState([])
 
-  function getParams() {
-    return {
-      page: Number(parsedData?.page) * Number(parsedData?.limit) || 0,
-      limit: Number(parsedData?.limit) || 10,
-      search: parsedData?.search || '',
-      sort: parsedData.sort || '',
-      order: parsedData.order || '',
-    }
-  }
-  const [requestParams, setRequestParams] = useState(getParams())
-console.log('object :>> ', setRequestParams)
+//   function getParams() {
+//     return {
+//       page: Number(parsedData?.page) * Number(parsedData?.limit) || 0,
+//       limit: Number(parsedData?.limit) || 10,
+//       search: parsedData?.search || '',
+//       sort: parsedData.sort || '',
+//       order: parsedData.order || '',
+//     }
+//   }
+//   const [requestParams, setRequestParams] = useState(getParams())
+// console.log('object :>> ', setRequestParams)
 
-  const { isLoading } = useQuery(['dashboard', requestParams], () => getDashboardStatistics, {
+  const { isLoading } = useQuery('dashboard', () => getDashboardStatistics, {
     select: (data) => data.then(result => setData(result.data.data)),
     staleTime: 240000,
   })
